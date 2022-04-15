2897bda3b51fa2c9d599f1ef9b92ade27dcc2624
Balazs Barcza <40806995+Balays33@users.noreply.github.com>
2022-04-15 15:32:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,25 @@ provider "aws" {
  region = "eu-west-1"
 }
 
-resource "aws_instance" "app_server" {
- ami = "ami-08ca3fed11864d6bb"
- instance_type = "t1.micro"
- key_name = "x19190638_balazs_barcza_NEW"
- tags = {
-   Name = "x19190638_balazs_barcza_terraform"
- }
+#resource "aws_instance" "app_server" {
+# ami = "ami-08ca3fed11864d6bb"
+# instance_type = "t1.micro"
+# key_name = "x19190638_balazs_barcza_NEW"
+# tags = {
+#   Name = "x19190638_balazs_barcza_terraform"
+# }
+#}
+#aws instance creation
+resource "aws_instance" "web_app" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.medium"
+  subnet_id     = "subnet-01f7f9c5b9fc99986"
+  associate_public_ip_address = true
+  key_name = "x19190638_balazs_barcza_NEW"
+  vpc_security_group_ids = ["sg-0268ecbf418effd94"]
+
+  tags = {
+    Name = "x19190638_balazs_barcza_NEW_webapp"
+  }
+  
 }
